ba268bd1f9e7296a27cdf8e59f0ed16e6b9a088d
Marius Albath <marius.albath@gmail.com>
2012-10-01 21:08:43 +0200

    Uebungen während der Stunde

--- Uebungen/demoexec.c
+++ Uebungen/demoexec.c
@@ -4,19 +4,21 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <stdio.h>
 
 int main () {
   pid_t pid;
   int status;
+printf( "Hello, my pid is %d\n", getpid() );
 
   if (fork() == 0) {
     printf ("Child executing \"/bin/date\"\n");
     execl ("/bin/date", "date", 0);
+    printf( "Hello I am a child, my pid is %d\n", pid);
 
     printf ("This should not happen!\n");
   }
   printf ("Parent waiting for child's dead\n");
+  printf( "Hello I am a child, my pid is %d\n", pid);
   pid=wait (&status);
   printf ("Parent: Child just died.\n");
   exit(0);
